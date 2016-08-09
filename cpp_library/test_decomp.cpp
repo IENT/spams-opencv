b3ff5ceea36496fcdddfa1f17b9680a83dbbef33
Christoph Weyer <herrzinter@yav.in>
2016-08-09 09:54:18 +0200

    Updated gitignore

--- cpp_library/test_decomp.cpp
+++ cpp_library/test_decomp.cpp
@@ -55,8 +55,8 @@ void test_lasso() {
     //TODO: Is there an easier way for initialization?
 
     double prD[m*p] = { 2, 1.8, 0.5 };    
-    Matrix<double> D(prD, m, p); 
- 
+    Matrix<double> D(prD, m, p);
+
     double prX[m*n] = { 1, 1, 0
                       , 2, 2, 1
                       , 3, 3, 2
@@ -81,58 +81,65 @@ void test_lasso() {
     delete spa;
 }
 
-void test_scale() {
-    // Copied from opencv doc and adapted
-    Mat image = imread("../data/boat.png", CV_LOAD_IMAGE_GRAYSCALE);
+
+Image<double>* read_image(char* filepath) {
+    Mat image = imread(filepath, 0);
 
     if(! image.data) {
         cout <<  "Could not open or find the image" << std::endl;
-        return;
     }
-    
+    //tESTWURST
+
     // Convert to double as SPAMS Image uses cblas under the hood and it
     // requires double
-    int l1 = image.cols * image.rows * image.channels();
-    double p1[l1];
-    for(int i = 0; i < l1; i++) {
-        p1[i] = (double) image.data[i];
+    int l = image.cols * image.rows * image.channels() * 2;
+    cout << image.cols << " " << image.rows << " " << image.channels() << std::endl;
+    double array[l];
+    for(int i = 0; i < l; i++) {
+        array[i] = (double) image.data[i];
     }
 
-    Image<double> I(p1, image.cols, image.rows, 
-                           image.channels());
+    return new Image<double>(array, image.cols, image.rows, image.channels());
+}
+
+void display_image(Image<double>* image) {
+    // Reconvert to unsigned char to display it with opencv
+    int l = image->width() * image->height() * image->numChannels();
+    unsigned char array[l];
+    for(int i = 0; i < l; i++) {
+        array[i] = (unsigned char) image->rawX()[i];
+    }
+    
+    Mat output(image->height(), image->width(), CV_LOAD_IMAGE_GRAYSCALE, array);
+    
+    namedWindow( "Image", WINDOW_AUTOSIZE );
+    imshow("Image", output);
+
+    waitKey(0);
+}
+
+void test_image_scale() {
+    Image<double>* image = read_image("../data/boat.png");
     
     // Scale dynamic to test some SPAMS Image function
-    I.scal(0.5);
+    image->scal(0.25);
     
-    // Reconvert to unsigned char to display it with opencv
-    int l2 = I.width() * I.height() * image.channels();
-    unsigned char p2[l2];
-    for(int i = 0; i < l2; i++) {
-        p2[i] = (unsigned char) I.rawX()[i];
-    }
-    
-    Mat output(I.width(), I.height(), CV_LOAD_IMAGE_GRAYSCALE, p2);
-    
-    cout << image.cols << "  " << I.width() << std::endl;
-    
-    namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
-    imshow("Display window", output);                   // Show our image inside it.
-
-    waitKey(0);                                          // Wait for a keystroke in the window
+    display_image(image);
 }
 
 void test_image() {
-
+    Image<double>* image = read_image("../data/boat.png");
+    display_image(image);
 }
 
 struct progs {
   const char *name;
   void (*prog)();
-} progs[] = { 
+} progs[] = {
     "omp", test_omp,
     "lasso", test_lasso,
     "image", test_image,
-    "scale", test_scale,
+    "scale", test_image_scale,
 };
 int main(int argc, char** argv) {
     for(int i = 1;i < argc;i++) {
